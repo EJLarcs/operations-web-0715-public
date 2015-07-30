c8e4e8e1fa4e840120b63361c1644892912ad1f6
EJLarcs <ejlarcombe@gmail.com>
2015-07-30 13:23:15 -0400

    speeds

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if speed < 40
+  true
+  elsif speed > 60 
+  true
+  else return false 
+  end
 end
-
-
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false 
 end
 	
-
-
+def sum_machine(x, y)
+  sum = (x..y)
+end
